23d35f06bbb4f0546072a8d7cfcfe12a277ab8c8
김정현 <djsk721@naver.com>
2023-10-11 09:56:49 +0900

    feat: add kaggle competition

---
+++ train.py
@@ -0,0 +1,286 @@
+import os
+import time
+from glob import glob
+from tqdm import tqdm
+
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import albumentations as A
+from scipy.ndimage.morphology import binary_dilation
+import segmentation_models_pytorch as smp
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+
+import torch
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms as T
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Util
+# function to get data
+def get_file_row(path):
+    """Produces ID of a patient, image and mask filenames from a particular path"""
+    path_no_ext, ext = os.path.splitext(path)
+    filename = os.path.basename(path)
+    
+    patient_id = '_'.join(filename.split('_')[:3]) # Patient ID in the csv file consists of 3 first filename segments
+    
+    return [patient_id, path, f'{path_no_ext}_mask{ext}']
+
+# Create Metrics
+def iou_pytorch(predictions: torch.Tensor, labels: torch.Tensor, e: float = 1e-7):
+    """Calculates Intersection over Union for a tensor of predictions"""
+    predictions = torch.where(predictions > 0.5, 1, 0)
+    labels = labels.byte()
+    
+    intersection = (predictions & labels).float().sum((1, 2))
+    union = (predictions | labels).float().sum((1, 2))
+    
+    iou = (intersection + e) / (union + e)
+    return iou
+
+def dice_pytorch(predictions: torch.Tensor, labels: torch.Tensor, e: float = 1e-7):
+    """Calculates Dice coefficient for a tensor of predictions"""
+    predictions = torch.where(predictions > 0.5, 1, 0)
+    labels = labels.byte()
+    
+    intersection = (predictions & labels).float().sum((1, 2))
+    return ((2 * intersection) + e) / (predictions.float().sum((1, 2)) + labels.float().sum((1, 2)) + e)
+
+def BCE_dice(output, target, alpha=0.01):
+    bce = torch.nn.functional.binary_cross_entropy(output, target)
+    soft_dice = 1 - dice_pytorch(output, target).mean()
+    return bce + alpha * soft_dice
+
+
+# DataSet Class 
+class MriDataset(Dataset):
+    def __init__(self, df, transform=None, mean=0.5, std=0.25):
+        super(MriDataset, self).__init__()
+        self.df = df
+        self.transform = transform
+        self.mean = mean
+        self.std = std
+        
+        
+    def __len__(self):
+        return len(self.df)
+        
+    def __getitem__(self, idx, raw=False):
+        row = self.df.iloc[idx]
+        img = cv2.imread(row['image_filename'], cv2.IMREAD_UNCHANGED)
+        mask = cv2.imread(row['mask_filename'], cv2.IMREAD_GRAYSCALE)
+        if raw:
+            return img, mask
+        
+        if self.transform:
+            augmented = self.transform(image=img, mask=mask)
+            image, mask = augmented['image'], augmented['mask']
+        
+        img = T.functional.to_tensor(img)
+        mask = mask // 255
+        mask = torch.Tensor(mask)
+        return img, mask
+
+# EarlyStopping
+class EarlyStopping():
+    """
+    Stops training when loss stops decreasing in a PyTorch module.
+    """
+    def __init__(self, patience:int = 6, min_delta: float = 0, weights_path: str = 'weights.pt'):
+        """
+        :param patience: number of epochs of non-decreasing loss before stopping
+        :param min_delta: minimum difference between best and new loss that is considered
+            an improvement
+        :paran weights_path: Path to the file that should store the model's weights
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_loss = float('inf')
+        self.weights_path = weights_path
+
+    def __call__(self, val_loss: float, model: torch.nn.Module):
+        if self.best_loss - val_loss > self.min_delta:
+            self.best_loss = val_loss
+            torch.save(model.state_dict(), self.weights_path)
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+    def load_weights(self, model: torch.nn.Module):
+        """
+        Loads weights of the best model.
+        :param model: model to which the weigths should be loaded
+        """
+        return model.load_state_dict(torch.load(self.weights_path))
+            
+
+# Read csv file of data
+files_dir = 'lgg-mri-segmentation/kaggle_3m'
+file_paths = glob(f'{files_dir}/*/*[0-9].tif')
+
+csv_path = 'lgg-mri-segmentation/kaggle_3m/data.csv'
+df = pd.read_csv(csv_path)
+
+# Missing values handling
+imputer = SimpleImputer(strategy="most_frequent")
+
+df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+
+filenames_df = pd.DataFrame((get_file_row(filename) for filename in file_paths), columns=['Patient', 'image_filename', 'mask_filename'])
+
+df = pd.merge(df, filenames_df, on="Patient")
+
+# Split data into Train, valid, test
+train_df, test_df = train_test_split(df, test_size=0.3)
+test_df, valid_df = train_test_split(test_df, test_size=0.5)
+
+# Transforming 
+transform = A.Compose([
+    A.ChannelDropout(p=0.3),
+    A.RandomBrightnessContrast(p=0.3),
+    A.ColorJitter(p=0.3),
+])
+
+train_dataset = MriDataset(train_df, transform)
+valid_dataset = MriDataset(valid_df)
+test_dataset = MriDataset(test_df)
+
+# DataLoader
+batch_size = 16
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1)
+
+# load model
+model = smp.Unet(
+    encoder_name="efficientnet-b7",
+    encoder_weights="imagenet",
+    in_channels=3,
+    classes=1,
+    activation='sigmoid',
+)
+model.to(device)
+
+# Training
+def training_loop(epochs, model, train_loader, valid_loader, optimizer, loss_fn, lr_scheduler):
+    history = {'train_loss': [], 'val_loss': [], 'val_IoU': [], 'val_dice': []}
+    early_stopping = EarlyStopping(patience=7)
+    
+    for epoch in range(1, epochs + 1):
+        start_time = time.time()
+        
+        running_loss = 0
+        model.train()
+        for i, data in enumerate(tqdm(train_loader)):
+            img, mask = data
+            img, mask = img.to(device), mask.to(device)
+            predictions = model(img)
+            predictions = predictions.squeeze(1)
+            loss = loss_fn(predictions, mask)
+            running_loss += loss.item() * img.size(0)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        
+        model.eval()
+        with torch.no_grad():
+            running_IoU = 0
+            running_dice = 0
+            running_valid_loss = 0
+            for i, data in enumerate(valid_loader):
+                img, mask = data
+                img, mask = img.to(device), mask.to(device)
+                predictions = model(img)
+                predictions = predictions.squeeze(1)
+                running_dice += dice_pytorch(predictions, mask).sum().item()
+                running_IoU += iou_pytorch(predictions, mask).sum().item()
+                loss = loss_fn(predictions, mask)
+                running_valid_loss += loss.item() * img.size(0)
+        train_loss = running_loss / len(train_loader.dataset)
+        val_loss = running_valid_loss / len(valid_loader.dataset)
+        val_dice = running_dice / len(valid_loader.dataset)
+        val_IoU = running_IoU / len(valid_loader.dataset)
+        
+        history['train_loss'].append(train_loss)
+        history['val_loss'].append(val_loss)
+        history['val_IoU'].append(val_IoU)
+        history['val_dice'].append(val_dice)
+        print(f'Epoch: {epoch}/{epochs} | Training loss: {train_loss} | Validation loss: {val_loss} | Validation Mean IoU: {val_IoU} '
+         f'| Validation Dice coefficient: {val_dice}')
+        
+        lr_scheduler.step(val_loss)
+        if early_stopping(val_loss, model):
+            early_stopping.load_weights(model)
+            break
+    model.eval()
+    return history
+
+loss_fn = BCE_dice
+optimizer = Adam(model.parameters(), lr=0.001)
+epochs = 60
+lr_scheduler = ReduceLROnPlateau(optimizer=optimizer, patience=2,factor=0.2)
+
+history = training_loop(epochs, model, train_loader, valid_loader, optimizer, loss_fn, lr_scheduler)
+
+# Test Evaluation
+with torch.no_grad():
+    running_IoU = 0
+    running_dice = 0
+    running_loss = 0
+    for i, data in enumerate(test_loader):
+        img, mask = data
+        img, mask = img.to(device), mask.to(device)
+        predictions = model(img)
+        predictions = predictions.squeeze(1)
+        running_dice += dice_pytorch(predictions, mask).sum().item()
+        running_IoU += iou_pytorch(predictions, mask).sum().item()
+        loss = loss_fn(predictions, mask)
+        running_loss += loss.item() * img.size(0)
+    loss = running_loss / len(test_dataset)
+    dice = running_dice / len(test_dataset)
+    IoU = running_IoU / len(test_dataset)
+    
+    print(f'Tests: loss: {loss} | Mean IoU: {IoU} | Dice coefficient: {dice}')
+
+
+# Testing
+width = 3
+columns = 10
+n_examples = columns * width
+
+fig, axs = plt.subplots(columns, width, figsize=(7*width , 7*columns), constrained_layout=True)
+red_patch = mpatches.Patch(color='red', label='The red data')
+fig.legend(loc='upper right',handles=[
+    mpatches.Patch(color='red', label='Ground truth'),
+    mpatches.Patch(color='green', label='Predicted abnormality')])
+i = 0
+with torch.no_grad():
+    for data in test_loader:
+        image, mask = data
+        mask = mask[0]
+        if not mask.byte().any():
+            continue
+        image = image.to(device)
+        prediction = model(image).to('cpu')[0][0]
+        prediction = torch.where(prediction > 0.5, 1, 0)
+        prediction_edges = prediction - binary_dilation(prediction)
+        ground_truth = mask - binary_dilation(mask)
+        image[0, 0, ground_truth.bool()] = 1
+        image[0, 1, prediction_edges.bool()] = 1
+        
+        axs[i//width][i%width].imshow(image[0].to('cpu').permute(1, 2, 0))
+        if n_examples == i + 1:
+            break
+        i += 1
